f7551ab846809f404c82ebf474949ac218b820f9
compie67 <compie67@gmail.com>
2025-05-29 16:13:42 +0200

    Update fediversebridge.php

--- plugins/default/css/fediversebridge.php
+++ plugins/default/css/fediversebridge.php
@@ -2,11 +2,13 @@
 /**
  * plugins/default/css/fediversebridge.php
  * 🇳🇱 Kleine styling voor FediverseBridge opt-in pagina en adminoverzicht
- * Door Eric Redegeld – nlsociaal.nl
+ * 🇬🇧 Basic styling for FediverseBridge opt-in page and admin overview
+ *
+ * Gemaakt door Eric Redegeld – nlsociaal.nl
  */
 ?>
 <style>
-/* 🔐 Gebruiker opt-in blok */
+/* 🔐 Gebruiker opt-in blok / User opt-in box */
 .fediverse-optin-page {
     border: 1px solid #ddd;
     padding: 15px;
@@ -15,29 +17,34 @@
     border-radius: 6px;
 }
 
+/* Titel van opt-in box / Title inside opt-in box */
 .fediverse-optin-page h3 {
     margin-top: 0;
 }
 
+/* Formulierruimte onder titel / Spacing for form */
 .fediverse-optin-page form {
     margin-top: 10px;
 }
 
-/* 💼 Admin opt-in gebruikerslijst */
+/* 💼 Admin gebruikerslijst / Admin list of opted-in users */
 .fediverse-optin-admin-list {
     list-style-type: disc;
     padding-left: 20px;
 }
 
+/* Afstand tussen gebruikersitems / Spacing between list items */
 .fediverse-optin-admin-list li {
     margin-bottom: 6px;
 }
 
+/* Links in adminlijst / Links in admin list */
 .fediverse-optin-admin-list a {
     text-decoration: none;
     color: #0366d6;
 }
 
+/* Hovereffect op links / Hover effect on links */
 .fediverse-optin-admin-list a:hover {
     text-decoration: underline;
 }
