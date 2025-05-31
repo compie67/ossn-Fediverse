8e3b5f2f36be48dc3488396a084ebe0a25425136
Compie67 <compie67@gmail.com>
2025-05-31 22:54:21 +0200

    Update ossn.nl.php

--- locale/ossn.nl.php
+++ locale/ossn.nl.php
@@ -11,21 +11,29 @@ ossn_register_languages('nl', array(
     'fediversebridge:adminmenu'               => 'Fediverse Opt-in gebruikers',
 
     // Profile page opt-in
-    'fediversebridge:optin:profile:title'     => 'Fediverse Opt-in',
-    'fediversebridge:optin:profile:enabled'   => 'Status: Ingeschakeld',
-    'fediversebridge:optin:profile:disabled'  => 'Status: Uitgeschakeld',
-    'fediversebridge:user:summary'            => 'Gebruiker van nlsociaal.nl – geen bot, handmatig gepost',
-
-    // Buttons
-    'fediversebridge:optin:profile:enablebtn' => 'Inschakelen',
-    'fediversebridge:optin:profile:disablebtn'=> 'Uitschakelen',
+    'fediversebridge:optin:profile:title'      => 'Fediverse Opt-in',
+    'fediversebridge:optin:profile:enabled'    => '✅ Je doet mee aan het Fediverse.',
+    'fediversebridge:optin:profile:disabled'   => '❌ Je doet momenteel niet mee aan het Fediverse.',
+    'fediversebridge:optin:profile:enablebtn'  => 'Inschakelen',
+    'fediversebridge:optin:profile:disablebtn' => 'Uitschakelen',
+    'fediversebridge:optin:profile:identity'   => 'Jouw Fediverse-identiteit',
+    'fediversebridge:optin:profile:linktext'   => 'Deel deze link zodat anderen je kunnen volgen:',
+    'fediversebridge:optin:profile:username'   => 'Gebruikersnaam: %s',
+    'fediversebridge:optin:profile:followers'  => 'Volgers',
+    'fediversebridge:optin:profile:activity'   => 'Activiteit',
+    'fediversebridge:optin:profile:activity:type'    => 'Type',
+    'fediversebridge:optin:profile:activity:from'    => 'Van',
+    'fediversebridge:optin:profile:activity:to'      => 'Naar',
+    'fediversebridge:optin:profile:activity:content' => 'Inhoud',
 
     // Feedback messages
-    'fediversebridge:optin:profile:success'   => 'Fediverse opt-in is ingeschakeld voor %s.',
-    'fediversebridge:optin:profile:error'     => 'Fediverse opt-in is uitgeschakeld voor %s.',
+    'fediversebridge:optin:profile:success'    => 'Fediverse-opt-in is ingeschakeld voor %s.',
+    'fediversebridge:optin:profile:error'      => 'Fediverse-opt-in is uitgeschakeld voor %s.',
 
-    // Fallback
+    // Fallbacks
     'fediversebridge:nousers'                 => 'Geen gebruikers hebben opt-in ingeschakeld.',
+    'fediversebridge:nofollowers'             => 'Nog geen volgers.',
+    'fediversebridge:noactivity'              => 'Nog geen ontvangen likes of reacties.',
 
     // Footer link
     'fediversebridge:footerlink'              => 'Klik hier voor meer op nlsociaal.nl',
