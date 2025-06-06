758673671cde8f08f4112ae36188854f5ea24558
Compie67 <compie67@gmail.com>
2025-06-06 15:47:51 +0200

    Update optinusers.php

--- pages/admin/optinusers.php
+++ pages/admin/optinusers.php
@@ -1,16 +1,13 @@
 <?php
 /**
  * pages/admin/optinusers.php
- * Admin page loader for the Fediverse opt-in user overview
+ * 🇳🇱 Adminpagina loader voor Fediverse opt-in gebruikersoverzicht
+ * 🇬🇧 Admin page loader for Fediverse opt-in user overview
  */
 
 if (!ossn_isAdminLoggedin()) {
     ossn_error_page();
-    return;
 }
 
-// Load the admin view for opt-in users
 $content = ossn_plugin_view('fediversebridge/admin/optinusers');
-
-// Render the page with title
-echo ossn_view_page(ossn_print('fediversebridge:optinusers'), $content);
+echo ossn_view_page('🔐 Fediverse Opt-in Gebruikers', $content);
